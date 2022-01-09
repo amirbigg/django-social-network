aac2de15fa960c1e7ea4c2f61546faa0b82bd003
amir <amir@email.com>
2022-01-09 13:13:59 +0330

    add ep5

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-	username = forms.CharField()
-	email = forms.EmailField()
-	password = forms.CharField()
+	username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
