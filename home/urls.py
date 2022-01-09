e3d8cfef59409697002c00c4abb064699a2177c8
amir <amir@email.com>
2022-01-09 11:43:09 +0330

    add ep3

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.home, name='home'),
+	path('', views.HomeView.as_view(), name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
 
 
-def home(request):
-	return render(request, 'home/index.html')
+class HomeView(View):
+	def get(self, request):
+		return render(request, 'home/index.html')
+
+	def post(self, request):
+		return render(request, 'home/index.html')
