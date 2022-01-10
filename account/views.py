538f2c3379fb85b88c6363625153de9da365a6e2
amir <amir@email.com>
2022-01-10 11:21:41 +0330

    add ep7

--- account/views.py
+++ account/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 class RegisterView(View):
+	form_class = UserRegistrationForm
+	template_name = 'account/register.html'
+
 	def get(self, request):
-		form = UserRegistrationForm()
-		return render(request, 'account/register.html', {'form':form})
+		form = self.form_class()
+		return render(request, self.template_name, {'form':form})
 
 	def post(self, request):
-		pass
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			User.objects.create_user(cd['username'], cd['email'], cd['password'])
+			messages.success(request, 'you registered successfully', 'success')
+			return redirect('home:home')
+		return render(request, self.template_name, {'form':form})
