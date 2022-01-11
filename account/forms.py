83b54b14fed8d62ae769375dcb1438711fca6b17
amir <amir@email.com>
2022-01-11 10:57:52 +0330

    add ep9

--- account/forms.py
+++ account/forms.py
@@ -6,7 +6,8 @@ from django.core.exceptions import ValidationError
 class UserRegistrationForm(forms.Form):
 	username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-	password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+	password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+	password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 	def clean_email(self):
 		email = self.cleaned_data['email']
@@ -14,3 +15,11 @@ class UserRegistrationForm(forms.Form):
 		if user:
 			raise ValidationError('this email already exists')
 		return email
+
+	def clean(self):
+		cd = super().clean()
+		p1 = cd.get('password1')
+		p2 = cd.get('password2')
+
+		if p1 and p2 and p1 != p2:
+			raise ValidationError('password must match')
--- account/views.py
+++ account/views.py
@@ -17,7 +17,7 @@ class RegisterView(View):
 		form = self.form_class(request.POST)
 		if form.is_valid():
 			cd = form.cleaned_data
-			User.objects.create_user(cd['username'], cd['email'], cd['password'])
+			User.objects.create_user(cd['username'], cd['email'], cd['password1'])
 			messages.success(request, 'you registered successfully', 'success')
 			return redirect('home:home')
 		return render(request, self.template_name, {'form':form})
