01476c6cc0a2798f56ae7ed415aa25c0e976625b
amir <amir@email.com>
2022-01-11 10:43:25 +0330

    add ep8

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
 	username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
 	password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+	def clean_email(self):
+		email = self.cleaned_data['email']
+		user = User.objects.filter(email=email).exists()
+		if user:
+			raise ValidationError('this email already exists')
+		return email
