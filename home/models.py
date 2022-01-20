4ce04f75131aebfb179089dfdceec4f736f60af7
amir <amir@email.com>
2022-01-20 11:20:43 +0330

    add ep16

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	body = models.TextField()
+	slug = models.SlugField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return f'{self.slug} - {self.updated}'
