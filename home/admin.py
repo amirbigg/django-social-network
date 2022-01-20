38e932885cdf7177ae225427b051d50ce914f4c0
amir <amir@email.com>
2022-01-20 11:47:17 +0330

    add ep17

--- home/admin.py
+++ home/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('user', 'slug', 'updated')
+	search_fields = ('slug', 'body')
+	list_filter = ('updated',)
+	prepopulated_fields = {'slug':('body',)}
+	raw_id_fields = ('user',)
+
