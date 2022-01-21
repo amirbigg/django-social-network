64257cb97c674abb509d5c6e86ea3e75f2f5522c
amir <amir@email.com>
2022-01-21 11:38:53 +0330

    add ep20

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 urlpatterns = [
 	path('', views.HomeView.as_view(), name='home'),
 	path('post/<int:post_id>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_detail'),
+	path('post/delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 
 class HomeView(View):
@@ -13,3 +15,14 @@ class PostDetailView(View):
 	def get(self, request, post_id, post_slug):
 		post = Post.objects.get(pk=post_id, slug=post_slug)
 		return render(request, 'home/detail.html', {'post':post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+	def get(self, request, post_id):
+		post = Post.objects.get(pk=post_id)
+		if post.user.id == request.user.id:
+			post.delete()
+			messages.success(request, 'post deleted successfully', 'success')
+		else:
+			messages.error(request, 'you cant delete this post', 'danger')
+		return redirect('home:home')
