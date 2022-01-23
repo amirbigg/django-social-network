24c2a27cb07c00890c8364e1c55a42ae8ebbb0ba
amir <amir@email.com>
2022-01-23 10:26:48 +0330

    add ep21

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('', views.HomeView.as_view(), name='home'),
 	path('post/<int:post_id>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_detail'),
 	path('post/delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
+	path('post/update/<int:post_id>/', views.PostUpdateView.as_view(), name='post_update'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from .forms import PostUpdateForm
 
 
 class HomeView(View):
@@ -26,3 +27,30 @@ class PostDeleteView(LoginRequiredMixin, View):
 		else:
 			messages.error(request, 'you cant delete this post', 'danger')
 		return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+	form_class = PostUpdateForm
+
+	def dispatch(self, request, *args, **kwargs):
+		post = Post.objects.get(pk=kwargs['post_id'])
+		if not post.user.id == request.user.id:
+			messages.error(request, 'you cant update this post', 'danger')
+			return redirect('home:home')
+		return super().dispatch(request, *args, **kwargs)
+
+	def get(self, request, post_id):
+		post = Post.objects.get(pk=post_id)
+		form = self.form_class(instance=post)
+		return render(request, 'home/update.html', {'form':form})
+
+	def post(self, request, post_id):
+		pass
+
+
+
+
+
+
+
+
