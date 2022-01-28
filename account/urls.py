4dad2adde0baea87e1b3df20f02bbcbf31c6ea00
amir <amir@email.com>
2022-01-28 11:00:18 +0330

    add ep26

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	path('register/', views.UserRegisterView.as_view(), name='user_register'),
 	path('login/', views.UserLoginView.as_view(), name='user_login'),
 	path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
-	path('profile/<int:user_id>/', views.UserProfileView.as_view(), name='user_profile')
+	path('profile/<int:user_id>/', views.UserProfileView.as_view(), name='user_profile'),
+	path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
 ]
--- account/views.py
+++ account/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from home.models import Post
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 
 class UserRegisterView(View):
@@ -69,3 +71,9 @@ class UserProfileView(LoginRequiredMixin, View):
 		user = get_object_or_404(User, pk=user_id)
 		posts = Post.objects.filter(user=user)
 		return render(request, 'account/profile.html', {'user':user, 'posts':posts})
+
+
+class UserPasswordResetView(auth_views.PasswordResetView):
+	template_name = 'account/password_reset_form.html'
+	success_url = reverse_lazy('account:password_reset_done')
+	email_template_name = 'account/password_reset_email.html'
