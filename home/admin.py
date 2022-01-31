5569328dc1d8f5d0ea63f058e054259f197fa0ed
amir <amir@email.com>
2022-01-31 14:29:08 +0330

    add ep34

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -10,3 +10,8 @@ class PostAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug':('body',)}
 	raw_id_fields = ('user',)
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('user', 'post', 'created', 'is_reply')
+	raw_id_fields = ('user', 'post', 'reply')
--- home/models.py
+++ home/models.py
@@ -18,3 +18,15 @@ class Post(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('home:post_detail', args=(self.id, self.slug))
+
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomments')
+	reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
+	is_reply = models.BooleanField(default=False)
+	body = models.TextField(max_length=400)
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f'{self.user} - {self.body[:30]}'
